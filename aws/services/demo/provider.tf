e641a9e4523bb3ff4634aba7d432dc03389c6b71
clops-veerakannan <94616605+clops-veerakannan@users.noreply.github.com>
2023-09-25 11:15:37 +0530

    Update provider.tf

--- aws/services/demo/provider.tf
+++ aws/services/demo/provider.tf
@@ -10,6 +10,6 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "ap-south-1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  access_key = "AKIA5NNMYBVTDAEXLCZ4"         #var.aws_access_key_id
+  secret_key = "jzU8IS09gL73wG3gWTtb7xfeqbickJM0iX4CP8z"    #var.aws_secret_access_key
 }
